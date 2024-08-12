f5385e7c07ef147be2a1a287a0244942b66bbb57
AlexandrKozhekin <alexandrkozhekin@gmail.com>
2024-08-12 17:07:06 +0300

    changes main method

--- src/main/java/by/solmed/assistant/AssistantApplication.java
+++ src/main/java/by/solmed/assistant/AssistantApplication.java
@@ -1,14 +1,25 @@
 package by.solmed.assistant;
 
-import by.solmed.assistant.ui.button.SaveClientButton;
-import by.solmed.assistant.ui.window.SaveClientWindow;
+import by.solmed.assistant.core.db.*;
+import by.solmed.assistant.ui.listeners.ClientListeners;
+import by.solmed.assistant.ui.listeners.StaffListeners;
+import by.solmed.assistant.ui.listeners.TicketListeners;
+import by.solmed.assistant.ui.window.ClientWindow;
+import by.solmed.assistant.ui.window.StaffWindow;
+import by.solmed.assistant.ui.window.TicketWindow;
 
 public class AssistantApplication {
 
+	private static final TicketDatabase ticketDatabase = new TicketDatabaseImpl();
+	private static final ClientDatabase clientDatabase = new ClientDatabaseImpl();
+	private static final ProcedureDatabase procedureDatabase = new ProcedureDatabaseImpl();
+	private static final StaffDatabase staffDatabase = new StaffDatabaseImpl();
+
 	public static void main(String[] args) throws Exception {
 
-		SaveClientWindow.run(new SaveClientButton(), 800, 600);
+		ClientWindow.run(new ClientListeners(clientDatabase), 1024, 500);
+		StaffWindow.run(new StaffListeners(staffDatabase), 1024, 500);
+		TicketWindow.run(new TicketListeners(ticketDatabase, clientDatabase, procedureDatabase, staffDatabase), 800, 600);
 
 	}
-
 }
